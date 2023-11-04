54fa2064bf380f47510cb9b0bbd57a5dfafcaafb
yashrajbharticybtekk <y.bharti@cybtekk.com>
2023-11-04 15:40:46 +0530

    wrkble

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+let cards = document.querySelector(".cards-container");
+let featured = document.querySelector(".featured-container");
+let pricing = document.querySelector(".pricing-container");
+
+window.addEventListener("resize", () => {
+  resizeCheck();
+});
+// ".cards-container-scroll, .featured-container-scroll, .pricing-container-scroll",
+const resizeCheck = () => {
+  if (window.matchMedia("(max-width: 1300px)").matches) {
+    !cards.classList.contains("cards-container-scroll") &&
+      cards.classList.add("cards-container-scroll");
+    !featured.classList.contains("featured-container-scroll") &&
+      featured.classList.add("featured-container-scroll");
+    !pricing.classList.contains("pricing-container-scroll") &&
+      pricing.classList.add("pricing-container-scroll");
+  } else {
+    cards.classList.contains("cards-container-scroll") &&
+      cards.classList.remove("cards-container-scroll");
+    featured.classList.contains("featured-container-scroll") &&
+      featured.classList.remove("featured-container-scroll");
+    pricing.classList.contains("pricing-container-scroll") &&
+      pricing.classList.remove("pricing-container-scroll");
+  }
+};
+resizeCheck();
 // fullpage customization
 $("#fullpage").fullpage({
   sectionsColor: ["#0e1012", "#E0E0E0", "#0e1012", "#0e1012", "#0e1012"],
