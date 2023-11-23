8e27988d97a315870c78aeddd1f755158e52add2
yashrajbharticybtekk <y.bharti@cybtekk.com>
2023-11-23 17:52:29 +0530

    code changes

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,18 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   let input = document.getElementById("check");
   let pane = document.querySelector(".pane");
+  let list = document.querySelectorAll(".hamburger-options li");
   input.addEventListener("change", () => {
     if (input.checked) {
-      //   pane.style.display = "block";
       pane.classList.add("slideIn");
     } else {
       pane.classList.remove("slideIn");
-      setTimeout(() => {
-        // pane.style.display = "none";
-      }, 3000);
     }
   });
-
+  list.forEach((li) => {
+    li.addEventListener("click", () => {
+      input.checked = false;
+      setTimeout(() => {
+        pane.classList.remove("slideIn");
+      }, 50);
+    });
+  });
   if (!!localStorage.getItem("userType")) {
     document.querySelector(".link-changer").href =
       "https://wrkble.com/dashboard";
